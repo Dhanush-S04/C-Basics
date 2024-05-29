d3fd55bb20e19c411099427c31ff800de2f7abff
Dhanush-S04 <151654501+Dhanush-S04@users.noreply.github.com>
2024-05-29 12:28:02 +0530

    Add files via upload

---
+++ if else/Positive and Negative number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Ente the number: ");
+    scanf("%d",&a);
+    if(a>0){
+        printf("Positive number");
+    }
+    else
+    {
+        printf("Negative number");
+    }
+}
