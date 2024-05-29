55c36391dda7d259360f235370b725b56c4cecb0
Dhanush-S04 <151654501+Dhanush-S04@users.noreply.github.com>
2024-05-29 11:14:15 +0530

    Add files via upload

---
+++ Relational operators/greater.c
@@ -0,0 +1,12 @@
+//Relational operator -- operand (operator) operand
+
+#include<stdio.h>
+int main(){
+    int a=5;
+    int b=10;
+    printf("%d",b>a);
+}
+
+
+
+
---
+++ Unary operator/Increment.c
@@ -0,0 +1,9 @@
+//Unary operator
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    a++;
+    printf("%d",a);
+    return 0;
+}
